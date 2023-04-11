0cd6c3848c95f88c12130d7b7a94c41a8aabf52c
TongLuong <tong.luong203@hcmut.edu.vn>
2023-04-11 22:47:46 +0700

    new interface

--- Form1.cs
+++ Form1.cs
@@ -14,41 +14,16 @@ namespace QuanLi
 {
     public partial class Form1 : Form
     {
-        private Rectangle pb1;
-        private Rectangle pb2;
-        private Rectangle originalFormSize;
-
         public Form1() => InitializeComponent();
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Order_Click(object sender, EventArgs e)
         {
-            originalFormSize = new Rectangle(this.Location.X, this.Location.Y, this.Size.Width, this.Height);
-            pb1 = new Rectangle(pbMenu.Location.X, pbMenu.Location.Y, pbMenu.Size.Width, pbMenu.Size.Height);
-            pb2 = new Rectangle(pbOrder.Location.X, pbOrder.Location.Y, pbOrder.Size.Width, pbOrder.Size.Height);
-        }
+            Label temp = new Label();
+            temp.Text = "this is a very long textttttt";
+            temp.AutoSize = true;
 
-        private void resizeControl(Rectangle r, Control c)
-        {
-            double xRatio = (double)r.Width / (double)originalFormSize.Width;
-            double yRatio = (double)r.Height / (double)originalFormSize.Height;
-
-            double xLocRatio = (double)r.Width / (double)r.X;
-            double yLocRatio = (double)r.Height / (double)r.Y;
-
-            int newX = (int)(r.X * xLocRatio);
-            int newY = (int)(r.Y * yLocRatio);
-
-            int newWidth = (int)(r.Width * xRatio);
-            int newHeight = (int)(r.Height * yRatio);
-
-            //c.Location = new Point(newX, newX);
-            //c.Size = new Size(newWidth, newHeight);
-        }
-
-        private void Form1_Resize(object sender, EventArgs e)
-        {
-            resizeControl(pb1, pbMenu);
-            resizeControl(pb2, pbOrder);
+            flowOrderName.Controls.Add(temp);
+            flowOrderName.SetFlowBreak(temp, true);
         }
     }
 }
