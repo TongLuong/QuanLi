13ffd7b85f4d63be17d9c189145f9e78d9e30ed1
TongLuong <tong.luong203@hcmut.edu.vn>
2023-04-21 09:44:31 +0700

    coded exitting form

--- ExittingForm.cs
+++ ExittingForm.cs
@@ -12,9 +12,41 @@ namespace QuanLi
 {
     public partial class ExittingForm : Form
     {
+        public enum CustomDialogResult
+        {
+            SAVE,
+            DONTSAVE,
+            CANCEL
+        }
+
+        private CustomDialogResult result;
+        public CustomDialogResult Result
+        {
+            get => result;
+            set => result = value;
+        }
+
         public ExittingForm()
         {
             InitializeComponent();
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            result = CustomDialogResult.SAVE;
+            this.Close();
+        }
+
+        private void DSave_Click(object sender, EventArgs e)
+        {
+            result = CustomDialogResult.DONTSAVE;
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            result = CustomDialogResult.CANCEL;
+            this.Close();
         }
     }
 }
