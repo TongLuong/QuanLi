d7bd5a26888f4f9c58839e1aabe38ce87123439b
TongLuong <tong.luong203@hcmut.edu.vn>
2023-04-23 22:39:39 +0700

    deleted HotKeys.cs

--- Form1.cs
+++ Form1.cs
@@ -792,5 +792,23 @@ namespace QuanLi
             return UnregisterHotKey(hWnd, id);
         }
     }
+
+    public partial class Form1 : Form
+    {
+        private KeyHandler keyHandler;
+        public const int WM_HOTKEY_MSG_ID = 0x0312;
+        private void HandleHotKey()
+        {
+            caretaker.Undo();
+        }
+
+        protected override void WndProc(ref Message mes)
+        {
+            if (mes.Msg == WM_HOTKEY_MSG_ID)
+                HandleHotKey();
+
+            base.WndProc(ref mes);
+        }
+    }
     #endregion
 }
